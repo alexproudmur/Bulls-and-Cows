18392147e787285a6a417f26d47327e149253614
alexproudmur <71935537+alexproudmur@users.noreply.github.com>
2021-02-09 21:05:22 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // инициализация сканнера
+        // инициализация сканера
         final Scanner scanner = new Scanner(System.in);
 
         try {
